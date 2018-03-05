58c5229a15e13cc34add16366ce502788b51346c
PHOTONINT\arjun_mu <arjun.murali@photoninfotech.net>
2018-03-05 19:20:27 +0530

    firebase credentials and project setup

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CREDENTIALS={
+    apiKey: "AIzaSyCeMWC6WKs4ZU8mD9z3r89x_na4YRHcXBg",
+    authDomain: "ioniccrud-6531b.firebaseapp.com",
+    databaseURL: "https://ioniccrud-6531b.firebaseio.com",
+    projectId: "ioniccrud-6531b",
+    storageBucket: "",
+    messagingSenderId: "1077829961257"
+}
